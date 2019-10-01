04fca6cbbdecfcab2b70c3a68e0f3298acf548af
Coel Tyrell <coel316@yahoo.com>
2019-10-01 14:53:28 -0700

    updated app.js, index and style.css. 1st question

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//var number = 4;
+//var string = 'string';
+//var boolean = true;
+
+/* var likesStrangers = prompt('Do you like strangers ?');
+likesStrangers = likesStrangers.toLowerCase();
+
+if (likesStrangers === 'yes') {
+  alert('Me too');
+} else {
+  alert ('Go meet some');
+}
+
+if(typeof string === 'string'){
+  alert ('I am') + likesStrangers;
+}
+*/
+
+var nycStreets = prompt('Did I grow up in Florida');
+nycStreets = nycStreets.toLowerCase();
+
+if (nycStreets === 'yes' || nycStreets === 'y') {
+  alert('I didnt, Check my about me page');
+} else {
+  alert('Correct! I grew up in NYC');
+}
