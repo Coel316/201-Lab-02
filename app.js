8b142f293473be69dea4164994b53c48ba257120
Coel Tyrell <coel316@yahoo.com>
2019-10-01 19:16:44 -0700

    added console.log messages

--- app.js
+++ app.js
@@ -25,8 +25,10 @@ var nycStreets = prompt('Did I grow up in Florida ?');
 nycStreets = nycStreets.toLowerCase();
 
 if (nycStreets === 'yes' || nycStreets === 'y') {
+  //console.log('I didnt, Check my about me page');
   alert('I didnt, Check my about me page');
 } else {
+  //console.log('Correct! I grew up in NYC');
   alert('Correct! I grew up in NYC');
 }
 
@@ -34,8 +36,10 @@ var likeTotravel = prompt ('Do I like to travel ?');
 likeTotravel = likeTotravel.toLowerCase();
 
 if (likeTotravel === 'yes' || likeTotravel === 'y') {
+  //console.log('You\'re correct. Been many places');
   alert('You\'re correct. Been many places');
 } else {
+  //console.log('Truth is - I love to travel');
   alert('Truth is - I love to travel');
 }
 
@@ -43,8 +47,10 @@ var likeTechnology = prompt('Do I like technology ?');
 likeTechnology = likeTechnology.toLowerCase();
 
 if (likeTechnology === 'no' || likeTechnology === 'n') {
+  //console.log('Check out my About me page');
   alert('Check out my About me page');
 } else {
+  //console.log('Technology is part of the future');
   alert('Technology is part of the future');
 }
 
@@ -52,8 +58,10 @@ var workFormilitary = prompt('Have I ever worked for the Military ?');
 workFormilitary = workFormilitary.toLowerCase();
 
 if (workFormilitary === 'yes' || workFormilitary === 'y') {
+  //console.log('Correct, I was in the best branch !');
   alert('Correct, I was in the best branch !');
 } else {
+  //console.log('Think again, I did');
   alert('Think again, I did');
 }
 
@@ -61,8 +69,10 @@ var codedWithbasic = prompt('Have I ever coded in BASIC ?');
 codedWithbasic = codedWithbasic.toLowerCase();
 
 if (codedWithbasic === 'yes' || codedWithbasic === 'y') {
+  //console.log('True.  BASIC was the stuff.');
   alert('True.  BASIC was the stuff.');
 } else {
+  //console.log('I did too code with BASIC');
   alert('I did too code with BASIC');
 }
 
